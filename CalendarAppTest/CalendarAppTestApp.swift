74420f56f833288c38910f4d2bbc9a36fc349acc
Patricia <patysiq@yahoo.com.br>
2021-05-10 10:26:50 -0300

    Initial Commit

---
+++ CalendarAppTest/CalendarAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAppTestApp.swift
+//  CalendarAppTest
+//
+//  Created by PATRICIA S SIQUEIRA on 10/05/21.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
